450531abc63efcccd8052b5895b4a2509cfe8c89
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-10-07 18:05:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 // Code your solution here
-// const findMatching = drivers.filter(function(driver) {
-//   console.log(driver)
-// });
-//
 function findMatching(drivers, driver){
   return drivers.filter(function(driverName){
     return driverName.toLowerCase() === driver.toLowerCase();
