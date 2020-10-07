1bf954819e0199097facd8a732cf54a41fbd21ff
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-10-07 15:31:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 //   console.log(driver)
 // });
 //
-function findMatching(drivers, driver){
-  // console.log(drivers, driver)
-  return drivers.filter(function(name){
-    return name.toLowerCase() === driver.toLowerCase();
-    // console.log(name.toLowerCase())
-  });
-}
+// function findMatching(drivers, driver){
+//   // console.log(drivers, driver)
+//   return drivers.filter(function(name){
+//     return name.toLowerCase() === driver.toLowerCase();
+//     // console.log(name.toLowerCase())
+//   });
+// }
 const findMatching = drivers.filter(driver => {
   console.log(drivers, driver)
   return driver
