8f6bc1a2ac7bdc706f287d62c7cf740d362d1902
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-10-07 18:07:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,13 +4,20 @@
 //     return driverName.toLowerCase() === driver.toLowerCase();
 //   });
 // }
+
 //same as above
+// ES6 notation with explicit return
 // const findMatching = (drivers, driver) => {
 //   return drivers.filter(driverName => {
 //     return driverName.toLowerCase() === driver.toLowerCase();
 //   });
 // }
-//same as above
-const findMatching = (drivers, driver) => {
-  return drivers.filter(driverName => driverName.toLowerCase() === driver.toLowerCase());
-}
+
+//same as above with one implicit return
+// const findMatching = (drivers, driver) => {
+//   return drivers.filter(driverName => driverName.toLowerCase() === driver.toLowerCase());
+// }
+
+//same as above full implicit return
+const findMatching = (drivers, driver) =>
+  drivers.filter(driverName => driverName.toLowerCase() === driver.toLowerCase());
