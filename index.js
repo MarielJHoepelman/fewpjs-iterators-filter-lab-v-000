53fd0d1540158801a20e10a3462f0557ab7497e0
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-10-07 18:05:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // Code your solution here
-function findMatching(drivers, driver){
-  return drivers.filter(function(driverName){
-    return driverName.toLowerCase() === driver.toLowerCase();
-  });
-}
+// function findMatching(drivers, driver){
+//   return drivers.filter(function(driverName){
+//     return driverName.toLowerCase() === driver.toLowerCase();
+//   });
+// }
 //same as above
-const findMatching = (drivers, driver) => {
-  return drivers.filter(driverName => {
-    return driverName.toLowerCase() === driver.toLowerCase();
-  });
-}
+// const findMatching = (drivers, driver) => {
+//   return drivers.filter(driverName => {
+//     return driverName.toLowerCase() === driver.toLowerCase();
+//   });
+// }
 //same as above
 const findMatching = (drivers, driver) => {
   return drivers.filter(driverName => driverName.toLowerCase() === driver.toLowerCase());
